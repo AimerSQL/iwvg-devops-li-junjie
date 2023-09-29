2d7d8cd64d7bd2ab02f54efc0e855394626f995a
JunjieLi <dshen123456@gmail.com>
2023-09-29 18:27:41 +0200

    added FractionTest #5

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -57,35 +57,6 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
-    public boolean isProper() {
-        return Math.abs(numerator) < Math.abs(denominator);
-    }
-
-    public boolean isImproper() {
-        return Math.abs(numerator) >= Math.abs(denominator);
-    }
-
-    public boolean isEquivalent(Fraction other) {
-        return this.numerator * other.denominator == this.denominator * other.numerator;
-    }
-
-    public Fraction add(Fraction other) {
-        int commonDenominator = this.denominator * other.denominator;
-        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
-        return new Fraction(newNumerator, commonDenominator);
-    }
-
-    public Fraction multiply(Fraction other) {
-        int newNumerator = this.numerator * other.numerator;
-        int newDenominator = this.denominator * other.denominator;
-        return new Fraction(newNumerator, newDenominator);
-    }
-
-    public Fraction divide(Fraction other) {
-        int newNumerator = this.numerator * other.denominator;
-        int newDenominator = this.denominator * other.numerator;
-        return new Fraction(newNumerator, newDenominator);
-    }
     @Override
     public String toString() {
         return "Fraction{" +
@@ -104,6 +75,13 @@ public class Fraction {
         }
         Fraction other = (Fraction) obj;
         return numerator == other.numerator && denominator == other.denominator;
+    }
+
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + numerator;
+        result = 31 * result + denominator;
+        return result;
     }
 
 }
